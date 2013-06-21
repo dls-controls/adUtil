342efadef4f6bd3bb5ea0be1982aff94fdf542ca
Giles Knap <Giles.Knap@diamond.ac.uk>
2013-06-21 09:30:43 +0000

    adUtil: builder now working

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,10 @@
+from iocbuilder import Device, AutoSubstitution, Architecture, Xml
+from iocbuilder.arginfo import *
 
+from iocbuilder.modules.areaDetector import AreaDetector, _NDPluginBase, _NDFile, _NDFileBase, _ADBase
+    
+class gdaPlugins(Xml):
+    """This plugin instantiates a standard set of plugins for use by GDA:"""
+    TemplateFile = 'gdaPlugins.xml'  
+gdaPlugins.ArgInfo.descriptions["CAM"] = Ident("Areadetecor camera to connect to", _ADBase)  
 
