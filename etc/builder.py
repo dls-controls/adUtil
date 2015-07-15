9bb1bbe6cb9e737c7d68000db17a7b70fd6e874b
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-07-15 16:29:48 +0100

    Update iocbuilder support to use ADCore instead of old areaDetector

--- etc/builder.py
+++ etc/builder.py
@@ -1,69 +1,120 @@
 from iocbuilder import Device, AutoSubstitution, SetSimulation, Xml
 from iocbuilder.arginfo import *
 
-from iocbuilder.modules.areaDetector import AreaDetector, _ADBase, _ADBaseTemplate, _NDPluginProducerBase, _NDPluginBase
-from iocbuilder.modules.asyn import AsynIP
+from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, NDPluginBaseTemplate, includesTemplates, makeTemplateInstance
+from iocbuilder.modules.asyn import AsynIP, AsynPort
 
 class gdaPlugins(Xml):
     """This plugin instantiates a standard set of plugins for use by GDA:"""
     TemplateFile = 'gdaPlugins.xml'
-gdaPlugins.ArgInfo.descriptions["CAM"] = Ident("Areadetecor camera to connect to", _ADBase)
+gdaPlugins.ArgInfo.descriptions["CAM"] = Ident("Areadetecor camera to connect to", ADCore)
 
 class AdUtil(Device):
     '''Library dependencies for adUtil'''
-    Dependencies = (AreaDetector,)
+    Dependencies = (ADCore,)
     # Device attributes
     LibFileList = ['adUtil']
     DbdFileList = ['adUtilSupport']  
     AutoInstantiate = True      
 
+@includesTemplates(NDPluginBaseTemplate)
 class _BPM_calc(AutoSubstitution):
     """Template containing the records for an NDROI"""
     TemplateFile = 'BPM_calc.template'
 
-class BPM_calc(_NDPluginProducerBase):
+class BPM_calc(AsynPort):
     """This plugin selects a region of interest and optionally scales it to
     fit in a particular data type"""
+    # This tells xmlbuilder to use PORT instead of name as the row ID
+    UniqueName = "PORT"
+    Dependencies = (AdUtil,)    
     _SpecificTemplate = _BPM_calc
-    # NOTE: _NDPluginBase comes 2nd so we overwrite NDARRAY_PORT argInfo
-    ArgInfo = _SpecificTemplate.ArgInfo + _NDPluginBase.ArgInfo
-    Dependencies = (AdUtil,)
 
+    def __init__(self, PORT, NDARRAY_PORT, QUEUE = 2, BLOCK = 0, NDARRAY_ADDR = 0, BUFFERS = 50, MEMORY = 0, **args):
+        # Init the superclass (AsynPort)
+        self.__super.__init__(PORT)
+        # Update the attributes of self from the commandline args
+        self.__dict__.update(locals())
+        # Make an instance of our template
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
+
+    ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
+        PORT = Simple('Port name for the BPM_calc plugin', str),
+        QUEUE = Simple('Input array queue size', int),
+        BLOCK = Simple('Blocking callbacks?', int),
+        NDARRAY_PORT = Ident('Input array port', AsynPort),
+        NDARRAY_ADDR = Simple('Input array port address', int),
+        BUFFERS = Simple('Max buffers to allocate', int),
+        MEMORY = Simple('Max memory to allocate, should be maxw*maxh*nbuffer for driver and all attached plugins', int))
+
+    def Initialise(self):
+        print '# BPM_calcConfigure(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr, maxBuffers, maxMemory)' % self.__dict__
+        print 'BPM_calcConfigure("%(PORT)s", %(QUEUE)d, %(BLOCK)d, "%(NDARRAY_PORT)s", %(NDARRAY_ADDR)s, %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
+
+@includesTemplates(NDPluginBaseTemplate)
 class _FFT_calc(AutoSubstitution):
     TemplateFile = 'FFT_calc.template'
 
-class FFT_calc(_NDPluginProducerBase):
+class FFT_calc(AsynPort):
+    # This tells xmlbuilder to use PORT instead of name as the row ID
+    UniqueName = "PORT"
+    Dependencies = (AdUtil,)    
     _SpecificTemplate = _FFT_calc
-    ArgInfo = _SpecificTemplate.ArgInfo + _NDPluginBase.ArgInfo
-    Dependencies = (AdUtil,)
 
+    def __init__(self, PORT, NDARRAY_PORT, QUEUE = 2, BLOCK = 0, NDARRAY_ADDR = 0, BUFFERS = 50, MEMORY = 0, **args):
+        # Init the superclass (AsynPort)
+        self.__super.__init__(PORT)
+        # Update the attributes of self from the commandline args
+        self.__dict__.update(locals())
+        # Make an instance of our template
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
+
+    ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
+        PORT = Simple('Port name for the FFT_calc plugin', str),
+        QUEUE = Simple('Input array queue size', int),
+        BLOCK = Simple('Blocking callbacks?', int),
+        NDARRAY_PORT = Ident('Input array port', AsynPort),
+        NDARRAY_ADDR = Simple('Input array port address', int),
+        BUFFERS = Simple('Max buffers to allocate', int),
+        MEMORY = Simple('Max memory to allocate, should be maxw*maxh*nbuffer for driver and all attached plugins', int))
+
+    def Initialise(self):
+        print '# FFT_calcConfigure(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr, maxBuffers, maxMemory)' % self.__dict__
+        print 'FFT_calcConfigure("%(PORT)s", %(QUEUE)d, %(BLOCK)d, "%(NDARRAY_PORT)s", %(NDARRAY_ADDR)s, %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
+
+@includesTemplates(NDPluginBaseTemplate)
 class _NDCircularBuff(AutoSubstitution):
     TemplateFile = 'NDCircularBuff.template'
 
-class NDCircularBuff(_NDPluginBase):
+class NDCircularBuff(AsynPort):
     '''This plugin provides a pre and post external trigger frame capture buffer'''
-    #Dependencies = (FFmpegServer,)    
+    # This tells xmlbuilder to use PORT instead of name as the row ID
+    UniqueName = "PORT"
+    Dependencies = (AdUtil,)    
     _SpecificTemplate = _NDCircularBuff
-    
-    def __init__(self, QUEUE = 50, BUFFERS = 1000, MEMORY = -1, Enabled = 1, **args):
-        # Init the superclass (_NDPluginBase)
-        args["Enabled"] = Enabled
-        self.__super.__init__(**args)
-        # Store the args
+        
+    def __init__(self, PORT, NDARRAY_PORT, QUEUE = 50, BLOCK = 0, NDARRAY_ADDR = 0, BUFFERS = 1000, MEMORY = 0, Enabled = 1, **args):
+        #args["Enabled"] = Enabled
+        # Init the superclass (AsynPort)
+        self.__super.__init__(PORT)
+        # Update the attributes of self from the commandline args
         self.__dict__.update(locals())
+        # Make an instance of our template
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
 
     # __init__ arguments
     # NOTE: _NDPluginBase comes 2nd so we overwrite NDARRAY_PORT argInfo
-    ArgInfo = _SpecificTemplate.ArgInfo + _NDPluginBase.ArgInfo + makeArgInfo(__init__,        
+    ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
+        PORT      = Simple('Port name for the FFT_calc plugin', str),
         Enabled   = Simple('Plugin Enabled at startup?', int),
         QUEUE     = Simple('Input array queue size', int),          
+        BLOCK     = Simple('Blocking callbacks?', int),
+        NDARRAY_PORT = Ident('Input array port', AsynPort),
+        NDARRAY_ADDR = Simple('Input array port address', int),
         BUFFERS   = Simple('Max number of buffers to allocate', int),
         MEMORY    = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
-            'for driver and all attached plugins', int))
-    Dependencies = [AdUtil,]
-    #def InitialiseOnce(self):
-    #    print "ffmpegServerConfigure(%(HTTP_PORT)d)" % self.__dict__                        
-                                                                        
+                           'for driver and all attached plugins', int))
+
     def Initialise(self):
         print '# NDCircularBuffConfigure(portName, queueSize, blockingCallbacks, '\
             'NDArrayPort, NDArrayAddr, maxBuffers, maxMemory)'    
@@ -72,33 +123,38 @@ class NDCircularBuff(_NDPluginBase):
             '"%(NDARRAY_ADDR)s", %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
 
 
+@includesTemplates(NDPluginBaseTemplate)
 class _NDReframe(AutoSubstitution):
     TemplateFile = 'NDReframe.template'
 
-class NDReframe(_NDPluginBase):
+class NDReframe(AsynPort):
     '''This plugin provides a pre and post external trigger frame capture buffer'''
-    #Dependencies = (FFmpegServer,)    
+    # This tells xmlbuilder to use PORT instead of name as the row ID
+    UniqueName = "PORT"
+    Dependencies = (AdUtil,)    
     _SpecificTemplate = _NDReframe
     
-    def __init__(self, QUEUE = 50, BUFFERS = 1000, MEMORY = -1, Enabled = 1, **args):
-        # Init the superclass (_NDPluginBase)
-        args["Enabled"] = Enabled
-        self.__super.__init__(**args)
-        # Store the args
+    def __init__(self, PORT, NDARRAY_PORT, QUEUE = 50, BLOCK = 0, NDARRAY_ADDR = 0, BUFFERS = 1000, MEMORY = 0, Enabled = 1, **args):
+        #args["Enabled"] = Enabled
+        # Init the superclass (AsynPort)
+        self.__super.__init__(PORT)
+        # Update the attributes of self from the commandline args
         self.__dict__.update(locals())
+        # Make an instance of our template
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
 
     # __init__ arguments
     # NOTE: _NDPluginBase comes 2nd so we overwrite NDARRAY_PORT argInfo
-    ArgInfo = _SpecificTemplate.ArgInfo + _NDPluginBase.ArgInfo + makeArgInfo(__init__,        
+    ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
+        PORT      = Simple('Port name for the FFT_calc plugin', str),
         Enabled   = Simple('Plugin Enabled at startup?', int),
         QUEUE     = Simple('Input array queue size', int),          
+        BLOCK     = Simple('Blocking callbacks?', int),
+        NDARRAY_PORT = Ident('Input array port', AsynPort),
+        NDARRAY_ADDR = Simple('Input array port address', int),
         BUFFERS   = Simple('Max number of buffers to allocate', int),
         MEMORY    = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
-            'for driver and all attached plugins.', int))
-    Dependencies = [AdUtil,]
-    #def InitialiseOnce(self):
-    #    print "ffmpegServerConfigure(%(HTTP_PORT)d)" % self.__dict__                        
-                                                                        
+                           'for driver and all attached plugins', int))                                                                        
     def Initialise(self):
         print '# NDReframeConfigure(portName, queueSize, blockingCallbacks, '\
             'NDArrayPort, NDArrayAddr, maxBuffers, maxMemory)'    
