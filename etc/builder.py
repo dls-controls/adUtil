efcf6c17da4a8b7a8324ed628b2bea37dba319a2
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-06-07 09:17:34 +0100

    Remove NDPluginCircularBuff and NDArrayRing
        * These have been imported into ADCore

--- etc/builder.py
+++ etc/builder.py
@@ -113,46 +113,6 @@ class FFTInt_calc(AsynPort):
         print '# FFT_IntegrationConfigure(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr, maxBuffers, maxMemory)' % self.__dict__
         print 'FFT_IntegrationConfigure("%(PORT)s", %(QUEUE)d, %(BLOCK)d, "%(NDARRAY_PORT)s", %(NDARRAY_ADDR)s, %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
 
-@includesTemplates(NDPluginBaseTemplate)
-class _NDCircularBuff(AutoSubstitution):
-    TemplateFile = 'NDCircularBuff.template'
-
-class NDCircularBuff(AsynPort):
-    '''This plugin provides a pre and post external trigger frame capture buffer'''
-    # This tells xmlbuilder to use PORT instead of name as the row ID
-    UniqueName = "PORT"
-    Dependencies = (AdUtil,)    
-    _SpecificTemplate = _NDCircularBuff
-        
-    def __init__(self, PORT, NDARRAY_PORT, QUEUE = 50, BLOCK = 0, NDARRAY_ADDR = 0, BUFFERS = 1000, MEMORY = 0, ENABLED = 1, **args):
-        #args["Enabled"] = Enabled
-        # Init the superclass (AsynPort)
-        self.__super.__init__(PORT)
-        # Update the attributes of self from the commandline args
-        self.__dict__.update(locals())
-        # Make an instance of our template
-        makeTemplateInstance(self._SpecificTemplate, locals(), args)
-
-    # __init__ arguments
-    # NOTE: _NDPluginBase comes 2nd so we overwrite NDARRAY_PORT argInfo
-    ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
-        PORT      = Simple('Port name for the FFT_calc plugin', str),
-        ENABLED   = Simple('Plugin Enabled at startup?', int),
-        QUEUE     = Simple('Input array queue size', int),          
-        BLOCK     = Simple('Blocking callbacks?', int),
-        NDARRAY_PORT = Ident('Input array port', AsynPort),
-        NDARRAY_ADDR = Simple('Input array port address', int),
-        BUFFERS   = Simple('Max number of buffers to allocate', int),
-        MEMORY    = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
-                           'for driver and all attached plugins', int))
-
-    def Initialise(self):
-        print '# NDCircularBuffConfigure(portName, queueSize, blockingCallbacks, '\
-            'NDArrayPort, NDArrayAddr, maxBuffers, maxMemory)'    
-        print 'NDCircularBuffConfigure(' \
-            '"%(PORT)s", %(QUEUE)d, %(BLOCK)d, "%(NDARRAY_PORT)s", ' \
-            '"%(NDARRAY_ADDR)s", %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
-
 
 @includesTemplates(NDPluginBaseTemplate)
 class _NDReframe(AutoSubstitution):
@@ -192,6 +152,3 @@ class NDReframe(AsynPort):
         print 'NDReframeConfigure(' \
             '"%(PORT)s", %(QUEUE)d, %(BLOCK)d, "%(NDARRAY_PORT)s", ' \
             '"%(NDARRAY_ADDR)s", %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
-
-
-
