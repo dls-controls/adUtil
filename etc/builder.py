72c353e056571d0cff6c527a0323955de1163be1
Benjamin Bradnick <ben.bradnick@diamond.ac.uk>
2019-01-31 15:21:15 +0000

    Added ArgInfo to gdaPlugins to provide descriptions for arguments

--- etc/builder.py
+++ etc/builder.py
@@ -4,10 +4,48 @@ from iocbuilder.arginfo import *
 from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, NDPluginBaseTemplate, includesTemplates, makeTemplateInstance
 from iocbuilder.modules.asyn import AsynIP, AsynPort
 
-class gdaPlugins(Xml):
+class _gdaPlugins(Xml):
     """This plugin instantiates a standard set of plugins for use by GDA:"""
     TemplateFile = 'gdaPlugins.xml'
-gdaPlugins.ArgInfo.descriptions["CAM"] = Ident("Areadetecor camera to connect to", ADCore)
+
+class gdaPlugins(Device):
+
+    def __init__(
+        self, CAM, P, PORTPREFIX, PLUGINQUEUE=16, PLUGINBUFFERS=2,
+        HIST_SIZE=256, NCHANS=1024, XSIZE=1024, YSIZE=1024, ARRFTVL="UCHAR",
+        NELEMENTS=786432, DISPLAYQUEUE=2, ARRTYPE="Int8", HTTP_PORT=8080,
+        CentroidThreshold=50, ComputeProfiles=1, ComputeStatistics=0,
+        ComputeCentroid=0):
+
+        # I wish I could easily unpack dictionaries in Python 2...
+        _gdaPlugins(
+            CAM=CAM, P=P, PORTPREFIX=PORTPREFIX, PLUGINQUEUE=PLUGINQUEUE,
+            PLUGINBUFFERS=PLUGINBUFFERS, HIST_SIZE=HIST_SIZE, NCHANS=NCHANS,
+            XSIZE=XSIZE, YSIZE=YSIZE, ARRFTVL=ARRFTVL, NELEMENTS=NELEMENTS,
+            DISPLAYQUEUE=DISPLAYQUEUE, ARRTYPE=ARRTYPE, HTTP_PORT=HTTP_PORT,
+            CentroidThreshold=CentroidThreshold, ComputeProfiles=ComputeProfiles,
+            ComputeStatistics=ComputeStatistics, ComputeCentroid=ComputeCentroid)
+
+
+    ArgInfo = makeArgInfo(__init__,
+        CAM = Ident("Areadetecor camera to connect to", ADCore),
+        P = Simple("PV prefix for all plugins", str),
+        PORTPREFIX = Simple("Asyn port name prefix for each plugin", str),
+        PLUGINQUEUE = Simple("Size of queue for each plugin (except NDStdArrays)", int),
+        PLUGINBUFFERS = Simple("Size of buffer for each plugin", int),
+        HIST_SIZE = Simple("Size of histogram in NDStats plugins", int),
+        NCHANS = Simple("Number of elements in the NDStats waveforms for past statistics", int),
+        XSIZE = Simple("Size of NDStats X Histograms", int),
+        YSIZE = Simple("Size of NDStats Y Histograms", int),
+        ARRFTVL = Choice("Data format of NDStdArrays waveform", ["STRING", "CHAR", "UCHAR", "SHORT", "USHORT", "LONG", "ULONG", "FLOAT", "DOUBLE", "ENUM"]),
+        NELEMENTS = Simple("Size of NDStdArrays waveform (Should equal size number of pixels in image)", int),
+        DISPLAYQUEUE = Simple("Size of queue for NDStdArrays plugin", int),
+        ARRTYPE = Choice("Asyn Array type of NDStdSrrays waveform", ["Int8", "Int16", "Int32", "Float32", "Float64"]),
+        HTTP_PORT = Simple("Port used for ffmpegStream plugin", int),
+        CentroidThreshold = Simple("Threshold for calculating centroids in NDStats plugins", int),
+        ComputeProfiles = Choice("Toggle computation of profiles in NDStats plugins", [0, 1]),
+        ComputeStatistics = Choice("Toggle computation of basic statistics in NDStats plugins", [0, 1]),
+        ComputeCentroid = Choice("Toggle computation of centroids in NDStats plugins", [0, 1]))
 
 class AdUtil(Device):
     '''Library dependencies for adUtil'''
